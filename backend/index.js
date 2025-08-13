7e230345134bc2be68c48702a9b0749b21ab7317
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-13 21:33:59 +0500

    refactor: updated root route response and add 404 error handling

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 import "dotenv/config";
 import express from "express";
+
 const app = express();
+
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ resStatus: true, message: "Welcome to Notestracker API" });
+});
+
+app.all(/^/, (_, res) => {
+  res.status(404).json({ resStatus: false, error: "Route not found" });
 });
 
 app.listen(port, () => {
-    console.clear();
-  console.log(`Example app listening on port ${port}`);
+  console.clear();
+  console.log({ response: `Server running on http://localhost:${port}` });
 });
