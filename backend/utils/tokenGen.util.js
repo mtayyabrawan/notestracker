d09af6d30c8e99414a3d5421bc8138d187326d2f
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-15 13:50:07 +0500

    feat: added JWT_LOGIN_SECRET to environment variables and implement genLoginToken function

--- backend/utils/tokenGen.util.js
+++ backend/utils/tokenGen.util.js
@@ -1,10 +1,23 @@
 import jwt from "jsonwebtoken";
 
 const jwtValidationSecret = process.env.JWT_VALIDATION_SECRET;
+const jwtLoginSecret = process.env.JWT_LOGIN_SECRET;
 
 export function genVerificationToken(userId) {
   try {
     return jwt.sign({ id: userId }, jwtValidationSecret, { expiresIn: "1d" });
+  } catch (error) {
+    return new Error({
+      resStatus: false,
+      error: "Server error found",
+      message: error.message,
+    });
+  }
+}
+
+export function genLoginToken(userId, email) {
+  try {
+    return jwt.sign({ id: userId, email: email }, jwtLoginSecret);
   } catch (error) {
     return new Error({
       resStatus: false,
