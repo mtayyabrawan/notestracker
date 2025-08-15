82db6c2b019005d23d0a3551b17652570c5e3d7b
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-15 19:31:08 +0500

    feat:added backup code model

---
+++ backend/models/backupCode.model.js
@@ -0,0 +1,48 @@
+import { model, Schema } from "mongoose";
+
+const codeSchema = new Schema(
+  {
+    hash: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    used: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { _id: false },
+);
+
+const backupCodeSchema = new Schema({
+  code1: {
+    type: codeSchema,
+    required: true,
+  },
+  code2: {
+    type: codeSchema,
+    required: true,
+  },
+  code3: {
+    type: codeSchema,
+    required: true,
+  },
+  code4: {
+    type: codeSchema,
+    required: true,
+  },
+  code4: {
+    type: codeSchema,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+});
+
+const BackupCode = model("BackupCode", backupCodeSchema);
+
+export default BackupCode;
