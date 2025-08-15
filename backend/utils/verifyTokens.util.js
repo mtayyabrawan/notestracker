77be3f8b43d082fa3f4a23a4ebc383aad2cc5216
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-15 15:29:33 +0500

    feat: added verifyLoginToken function for JWT login token validation

--- backend/utils/verifyTokens.util.js
+++ backend/utils/verifyTokens.util.js
@@ -1,10 +1,23 @@
 import jwt from "jsonwebtoken";
 
 const jwtValidationSecret = process.env.JWT_VALIDATION_SECRET;
+const jwtLoginSecret = process.env.JWT_LOGIN_SECRET;
 
 export function verifyValidationToken(token) {
   try {
     return jwt.verify(token, jwtValidationSecret);
+  } catch (error) {
+    return new Error({
+      resStatus: false,
+      error: "Invalid or expired token",
+      message: error.message,
+    });
+  }
+}
+
+export function verifyLoginToken(token) {
+  try {
+    return jwt.verify(token, jwtLoginSecret);
   } catch (error) {
     return new Error({
       resStatus: false,
