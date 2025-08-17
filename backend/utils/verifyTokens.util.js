697ff76ef80a781083c09ef973067fef237e7a0a
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-17 11:23:04 +0500

    feat: added reset token verification util

--- backend/utils/verifyTokens.util.js
+++ backend/utils/verifyTokens.util.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 
 const jwtValidationSecret = process.env.JWT_VALIDATION_SECRET;
 const jwtLoginSecret = process.env.JWT_LOGIN_SECRET;
+const jwtResetSecret = process.env.JWT_RESET_SECRET;
 
 export function verifyValidationToken(token) {
   try {
@@ -18,6 +19,18 @@ export function verifyValidationToken(token) {
 export function verifyLoginToken(token) {
   try {
     return jwt.verify(token, jwtLoginSecret);
+  } catch (error) {
+    return new Error({
+      resStatus: false,
+      error: "Invalid or expired token",
+      message: error.message,
+    });
+  }
+}
+
+export function verifyResetToken(token) {
+  try {
+    return jwt.verify(token, jwtResetSecret);
   } catch (error) {
     return new Error({
       resStatus: false,
