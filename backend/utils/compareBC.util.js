08beded441a27e4ac01c6ae63c6ed781c240c027
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-17 18:55:14 +0500

    feat: added utility to compare bc with hashes

---
+++ backend/utils/compareBC.util.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcryptjs";
+
+function compareBackupCode(backupCode, bcHashes) {
+  for (let i = 0; i < 5; i++) {
+    if (bcrypt.compareSync(backupCode, bcHashes[i].hash) && !bcHashes[i].used) {
+      return { code: i + 1, matched: true };
+    }
+  }
+  return { code: 0, matched: false };
+}
+
+export default compareBackupCode;
