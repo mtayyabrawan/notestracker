8d29b10bea42f54ce683b8f7bf7d048982217e45
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-19 18:32:08 +0500

    feat: added SimpleFooter component and hide scrollbar in index.css

---
+++ src/components/common/SimpleFooter.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router";
+
+function SimpleFooter() {
+  return (
+    <footer className="flex w-full items-center justify-between bg-neutral-400 p-2 text-neutral-950">
+      <div className="font-medium">
+        <Link to="/" className="flex items-center space-x-1">
+          <img src="/notebook.svg" alt="logo" className="h-5 w-5" />
+          <span>Notestracker</span>
+        </Link>
+      </div>
+      <p className="text-xs">Copyright &copy; {new Date().getFullYear()}</p>
+      <div className="space-x-2 text-sm">
+        <Link to="/privacy-policy">Privacy Policy</Link>
+        <Link to="/terms-and-conditions">Terms & Conditions</Link>
+      </div>
+    </footer>
+  );
+}
+
+export default SimpleFooter;
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router";
+import SimpleFooter from "../components/common/SimpleFooter";
 
 function AuthLayout() {
   return (
-    <div>
-      This is Auth Layout
-      <Outlet />
+    <div className="w-full">
+      <main className="min-h-[calc(100vh-2.5rem))] w-full">
+        <Outlet />
+      </main>
+      <SimpleFooter />
     </div>
   );
 }
