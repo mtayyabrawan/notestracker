beb2d6415943e26292144506dc579db350c97660
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-20 18:02:37 +0500

    feat: enhanced Login component with form fields and navigation links, updated AuthLayout for centering, and added global transition styles in CSS

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,7 +4,7 @@ import SimpleFooter from "../components/common/SimpleFooter";
 function AuthLayout() {
   return (
     <div className="w-full">
-      <main className="min-h-[calc(100vh-2.5rem))] w-full">
+      <main className="flex min-h-[calc(100vh-2.5rem))] w-full items-center justify-center">
         <Outlet />
       </main>
       <SimpleFooter />
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <form className="w-96 space-y-6 rounded-lg p-4 text-sm shadow-[0px_0px_10px_0px_var(--color-neutral-500)] ring-[0.5px] ring-neutral-900">
+      <h1 className="text-center text-lg font-medium">Login Now</h1>
+      <div className="flex w-full flex-col items-center space-y-2">
+        <label htmlFor="email" className="w-full">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          autoComplete="off"
+          className="w-full rounded-md p-1.5 ring-[0.5px] ring-neutral-900 focus-visible:outline-hidden"
+        />
+      </div>
+      <div className="flex w-full flex-col items-center space-y-2">
+        <label htmlFor="password" className="w-full">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          autoComplete="off"
+          className="w-full rounded-md p-1.5 ring-[0.5px] ring-neutral-900 focus-visible:outline-hidden"
+        />
+      </div>
+      <button className="w-full rounded-md bg-neutral-900 px-2.5 py-1.5 font-medium text-neutral-200">
+        Submit
+      </button>
+      <div className="flex w-full items-center justify-between text-xs">
+        <Link
+          to="/auth/register"
+          className="underline-offset-1 hover:underline"
+        >
+          Create new account
+        </Link>
+        <Link
+          to="/auth/forgot-password"
+          className="underline-offset-1 hover:underline"
+        >
+          Forgot Password
+        </Link>
+      </div>
+    </form>
+  );
 }
 
 export default Login;
