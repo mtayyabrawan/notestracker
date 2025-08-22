fdfbfa7d1e49fba6cc003d930e6ac82c175eb906
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-22 11:35:29 +0500

    feat: integrated form validation and error handling in Login component using react-hook-form and Zod

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 import SimpleFooter from "../components/common/SimpleFooter";
+import { Toaster } from "sonner";
 
 function AuthLayout() {
   return (
     <div className="w-full">
+      <Toaster position="top-center" theme="system" richColors={true} />
       <main className="flex min-h-[calc(100vh-2.5rem))] w-full items-center justify-center">
         <Outlet />
       </main>
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,32 +1,72 @@
 import { Link } from "react-router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import loginSchema from "../../schemas/loginSchema";
+import { toast } from "sonner";
+import type { LoginSchema } from "../../schemas/loginSchema";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: { email: "", password: "" },
+    resolver: zodResolver(loginSchema),
+  });
+
+  async function formSubmit(formdata: LoginSchema) {
+    console.log(formdata);
+    toast.success("Login successful!");
+    reset();
+  }
+
   return (
-    <form className="w-96 space-y-6 rounded-lg p-4 text-sm shadow-[0px_0px_10px_0px_var(--color-neutral-500)] ring-[0.5px] ring-neutral-900">
+    <form
+      className="w-96 space-y-6 rounded-lg p-4 text-sm shadow-[0px_0px_10px_0px_var(--color-neutral-500)] ring-[0.5px] ring-neutral-900"
+      onSubmit={handleSubmit(formSubmit)}
+      noValidate
+    >
       <h1 className="text-center text-lg font-medium">Login Now</h1>
       <div className="flex w-full flex-col items-center space-y-2">
         <label htmlFor="email" className="w-full">
           Email
         </label>
+        {errors.email && (
+          <span className="mx-auto w-[95%] text-right text-[10px] text-red-500">
+            {errors.email.message}
+          </span>
+        )}
         <input
           type="email"
           id="email"
           autoComplete="off"
-          className="w-full rounded-md p-1.5 ring-[0.5px] ring-neutral-900 focus-visible:outline-hidden"
+          {...register("email")}
+          className={`w-full rounded-md p-1.5 text-center ring-[0.5px] focus-visible:outline-hidden ${errors.email ? "ring-red-500 focus-visible:shadow-[0px_0px_8px_0px_var(--color-red-700)]" : "ring-neutral-900 focus-visible:shadow-[0px_0px_8px_0px_var(--color-neutral-700)]"}`}
         />
       </div>
       <div className="flex w-full flex-col items-center space-y-2">
         <label htmlFor="password" className="w-full">
           Password
         </label>
+        {errors.password && (
+          <span className="mx-auto w-[95%] text-right text-[10px] text-red-500">
+            {errors.password.message}
+          </span>
+        )}
         <input
           type="password"
           id="password"
           autoComplete="off"
-          className="w-full rounded-md p-1.5 ring-[0.5px] ring-neutral-900 focus-visible:outline-hidden"
+          {...register("password")}
+          className={`w-full rounded-md p-1.5 text-center ring-[0.5px] focus-visible:outline-hidden ${errors.password ? "ring-red-500 focus-visible:shadow-[0px_0px_8px_0px_var(--color-red-700)]" : "ring-neutral-900 focus-visible:shadow-[0px_0px_8px_0px_var(--color-neutral-700)]"}`}
         />
       </div>
-      <button className="w-full rounded-md bg-neutral-900 px-2.5 py-1.5 font-medium text-neutral-200">
+      <button
+        disabled={isSubmitting}
+        className="w-full cursor-pointer rounded-md bg-neutral-900 px-2.5 py-2 font-medium text-neutral-200 shadow-[0px_0px_5px_0px_var(--color-neutral-700)] hover:bg-neutral-800 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:bg-neutral-500"
+      >
         Submit
       </button>
       <div className="flex w-full items-center justify-between text-xs">
