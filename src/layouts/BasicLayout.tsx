a9f77ed7411114db098959bf9e3587efd07c3ab6
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-08-24 20:15:17 +0500

    feat: refactored BasicLayout to include Toaster, Navbar, and AdvancedFooter components

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router";
+import { Toaster } from "sonner";
+import AdvancedFooter from "../components/common/AdvancedFooter";
+import Navbar from "../components/common/Navbar";
 
 function BasicLayout() {
   return (
-    <div>
-      This is Basic Layout
-      <Outlet />
+    <div className="w-full">
+      <Toaster position="top-center" theme="system" richColors={true} />
+      <Navbar />
+      <main className="min-h-[calc(100vh-7.5rem))] w-full">
+        <Outlet />
+      </main>
+      <AdvancedFooter />
     </div>
   );
 }
