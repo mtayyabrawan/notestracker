53f4a821d8c7588a00073022fcfb617e5022a3f4
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-04 16:44:22 +0500

    fix: update utility

--- src/utils/errohan.util.ts
+++ src/utils/errohan.util.ts
@@ -1,18 +1,32 @@
-async function errohan(uri, options) {
+async function errohan(
+  uri: string,
+  options: {
+    method?: string;
+    credentials?: boolean;
+    body?: object;
+  } = {},
+): Promise<any> {
   try {
     const response = await fetch(uri, {
       method: options.method,
       credentials: options.credentials ? "include" : "omit",
       headers: { "Content-Type": "application/json" },
-      body: options.body && JSON.stringify({ ...options.body }),
+      body: options.body ? JSON.stringify(options.body) : undefined,
     });
     const parsedRes = await response.json();
     return parsedRes;
-  } catch (err) {
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        resStatus: false,
+        error: "Server connection failed",
+        long_error: err.message,
+      };
+    }
     return {
       resStatus: false,
       error: "Server connection failed",
-      long_error: err.message,
+      long_error: String(err),
     };
   }
 }
