94fb22048879c2c86ac73bc09f2d1ebadcf72fc0
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-05 11:14:29 +0500

    feat: added api to request forgot password

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,6 +1,8 @@
 import api_uri from "../constants/getURI.constant";
 import errohan from "../utils/errohan.util";
+
 import type { LoginSchema } from "../schemas/loginSchema";
+import type { ForgotPasswordSchema } from "../schemas/forgotPasswordSchema";
 
 async function loginUser(credentials: LoginSchema) {
   return await errohan(`${api_uri}/auth/login`, {
@@ -10,6 +12,14 @@ async function loginUser(credentials: LoginSchema) {
   });
 }
 
-const authAPI = { loginUser };
+async function forgotPassword(data: ForgotPasswordSchema) {
+  return await errohan(`${api_uri}/auth/forgot-password`, {
+    method: "POST",
+    body: { email: data.email },
+    credentials: true,
+  });
+}
+
+const authAPI = { loginUser, forgotPassword };
 
 export default authAPI;
