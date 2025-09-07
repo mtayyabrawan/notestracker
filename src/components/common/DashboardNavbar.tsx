3fbd98525ff91fe4e09e04eb5a8b328c7516ec11
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-07 14:55:41 +0500

    fix: modified dashboard layout

--- src/components/common/DashboardNavbar.tsx
+++ src/components/common/DashboardNavbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 function DashboardNavbar() {
   return (
-    <header className="flex items-center justify-between bg-neutral-950 px-4 py-3 text-neutral-200">
+    <header className="mx-auto flex max-w-[1540px] items-center justify-between bg-neutral-950 px-4 py-3 text-neutral-200">
       <Link to="/" className="flex items-center gap-2">
         <img src="/notebook.svg" alt="logo" className="h-8 w-8" />
         <span className="text-lg font-semibold">Notestracker</span>
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -2,14 +2,18 @@ import { Toaster } from "sonner";
 import SimpleFooter from "../components/common/SimpleFooter";
 import { Outlet } from "react-router";
 import DashboardNavbar from "../components/common/DashboardNavbar";
+import Sidebar from "../components/Sidebar";
 
 function DashboardLayout() {
   return (
     <div className="w-full">
       <Toaster position="top-center" theme="system" richColors={true} />
       <DashboardNavbar />
-      <main className="min-h-[calc(100vh-6rem))] w-full">
-        <Outlet />
+      <main className="mx-auto flex h-[calc(100vh-6rem))] w-full max-w-[1540px] items-center justify-center">
+        <Sidebar />
+        <div className="h-full w-full overflow-x-hidden overflow-y-auto">
+          <Outlet />
+        </div>
       </main>
       <SimpleFooter />
     </div>
