7805ee7036aa70c82dd390028e3be00645a06909
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-12 10:16:40 +0500

    fix: corrected api request

--- src/api/notes.api.ts
+++ src/api/notes.api.ts
@@ -2,7 +2,7 @@ import api_uri from "../constants/getURI.constant";
 import errohan from "../utils/errohan.util";
 
 async function getNotes() {
-  return errohan(`${api_uri}/api/notes/all`, {
+  return errohan(`${api_uri}/notes/all`, {
     method: "GET",
     credentials: true,
   });
