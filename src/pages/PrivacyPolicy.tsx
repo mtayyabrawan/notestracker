a066efbe2eef135c7d70a8f635d2c5e04ea2991b
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-15 13:20:03 +0500

    feat: added privacy policy page content

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,5 +1,71 @@
 function PrivacyPolicy() {
-  return <div>PrivacyPolicy</div>;
+  return (
+    <div className="space-y-4">
+      <div>
+        <h1 className="text-center text-xl font-medium" id="about-us">
+          Privacy Policy
+        </h1>
+      </div>
+      <div className="px-20 py-5 leading-[2.5]">
+        <p className="text-justify text-xs leading-[2.5] font-medium text-pretty text-neutral-700">
+          Effective Date: 15<sup>th</sup> September, 2025
+        </p>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          At NotesTracker, your privacy is importance to us. This Privacy Policy
+          outlines how we collect, use, and protect your information when you
+          use our services.
+        </p>
+        <h2 className="font-medium">1. Information We Collect</h2>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          We collect personal information when you create an account, including
+          your name, email address, and any notes you choose to store.
+        </p>
+        <h2 className="font-medium">2. How We Use Your Information</h2>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          We use the information we collect to:
+        </p>
+        <ol className="ml-8 list-decimal text-justify text-sm leading-[2.5] text-pretty">
+          <li>Provide and maintain our services</li>
+          <li>Notify you about changes to our services</li>
+          <li>Allow you to participate in interactive features</li>
+          <li>Provide customer support</li>
+          <li>
+            Gather analysis or valuable information so that we can improve our
+            services
+          </li>
+        </ol>
+        <h2 className="font-medium">3. Data Security</h2>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          We implement a variety of security measures to maintain the safety of
+          your personal information. Your data is stored securely, and we take
+          precautions to protect it from unauthorized access.
+        </p>
+        <h2 className="font-medium">4. Your Rights</h2>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          You have the right to access, correct, or delete your personal
+          information at any time. You can also request that we restrict the
+          processing of your personal information.
+        </p>
+        <h2 className="font-medium">5. Changes to This Privacy Policy</h2>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          We may update our Privacy Policy from time to time. We will notify you
+          of any changes by posting the new Privacy Policy on this page. You are
+          advised to review this Privacy Policy periodically for any changes.
+        </p>
+        <h2 className="font-medium">6. Contact Us</h2>
+        <p className="text-justify text-sm leading-[2.5] text-pretty">
+          If you have any questions about this Privacy Policy, please contact us
+          at:
+        </p>
+        <p className="ml-4 text-justify text-xs leading-[2.5] font-medium text-pretty text-neutral-700">
+          Email:{" "}
+          <a href="mailto:tayyabpasha1918@gmail.com" target="_blank">
+            tayyabpasha1918@gmail.com
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default PrivacyPolicy;
