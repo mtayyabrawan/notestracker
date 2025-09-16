6041e5f055d2e637914ea103a2618ab8e7fd8984
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-16 20:43:21 +0500

    feat: wrapped whole app in auth provider and created hook for auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import BasicRouter from "./routers/BasicRouter.route";
+import AuthProvider from "./store/providers/AuthProvider";
 
 function App() {
-  return <BasicRouter />;
+  return (
+    <AuthProvider>
+      <BasicRouter />
+    </AuthProvider>
+  );
 }
 
 export default App;
