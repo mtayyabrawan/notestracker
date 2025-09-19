1a3d86b65d8918cde57b5ffd0b48db7fd162d7b5
mtayyabrawan <mtayyabawan1918@gmail.com>
2025-09-19 09:32:51 +0500

    fix: modified loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,14 @@
+import { IconDots } from "@tabler/icons-react";
+
 function Loader() {
   return (
-    <div className="fixed top-0 left-1/2 z-50 flex h-full w-full max-w-[1540px] -translate-x-1/2 items-center justify-center bg-neutral-100">
-      <div>Loading...</div>
+    <div className="fixed top-0 left-1/2 z-50 flex h-full w-full max-w-[1540px] -translate-x-1/2 flex-col items-center gap-3 bg-neutral-100/50">
+      <div className="mt-48 font-medium">NotesTracker</div>
+      <div className="flex animate-pulse">
+        <IconDots />
+        <IconDots />
+        <IconDots />
+      </div>
     </div>
   );
 }
